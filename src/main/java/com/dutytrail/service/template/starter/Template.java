01afa70cdc5a30dcff6151fc3ba4fa1b84a45a6f
joan.tolos <joan.tolos@thomsonreuters.com>
2016-04-22 11:05:14 +0200

    Bug Solved

--- src/main/java/com/dutytrail/service/template/starter/Template.java
+++ src/main/java/com/dutytrail/service/template/starter/Template.java
@@ -1,6 +1,5 @@
 package com.dutytrail.service.template.starter;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -14,9 +13,6 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = {"com.dutytrail.service.template"})
 @EnableFeignClients(basePackages = {"com.dutytrail.service.template"})
 public class Template {
-
-    @Value("${ping.alive}")
-    private String configPingAlive;
 
     public static void main(String[] args) {
         SpringApplication.run(Template.class, args);
