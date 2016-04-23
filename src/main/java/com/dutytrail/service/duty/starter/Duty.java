7f84ac763185d6eed0ebb9fd4e7a58dace402f05
joan.tolos <joan.tolos@thomsonreuters.com>
2016-04-23 19:56:52 +0200

    Duty service property file

--- src/main/java/com/dutytrail/service/duty/starter/Duty.java
+++ src/main/java/com/dutytrail/service/duty/starter/Duty.java
@@ -1,11 +1,15 @@
 package com.dutytrail.service.duty.starter;
 
+import com.dutytrail.service.duty.server.DutyService;
+import com.netflix.config.ConfigurationManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.Method;
 
 @Configuration
 @EnableAutoConfiguration
@@ -15,7 +19,14 @@ import org.springframework.context.annotation.Configuration;
 public class Duty {
 
     public static void main(String[] args) {
+        configureHystrix();
         SpringApplication.run(Duty.class, args);
+    }
+
+    private static void configureHystrix() {
+        for (Method method : DutyService.class.getMethods()) {
+            ConfigurationManager.getConfigInstance().setProperty(String.format("hystrix.command.%s.execution.isolation.thread.timeoutInMilliseconds", method.getName()), 15000);
+        }
     }
 
 }
